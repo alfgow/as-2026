fcce1efc24c7cc6dff8bf79ce4888d39a5e15917
alfonsovillanueva <alfonso@villanuevagarcia.com>
2025-09-22 23:54:42 -0600

    ok

--- Backend/admin/Controllers/PolizaController.php
+++ Backend/admin/Controllers/PolizaController.php
@@ -765,7 +765,7 @@ class PolizaController
         $plantillasContrato = [
             'normal_pf' => 'Contrato_Normal_PF 2025.docx',
             'os_pf'     => 'Contrato_ObligadoSolidario_PF 2025.docx',
-            'fiador_pf'    => 'Contrato_Normal_PF 2025.docx',
+            'fiador_pf'    => 'Contrato_Fiador_PF 2025.docx',
             'pmoral'    => 'Contrato_Persona_Moral.docx',
         ];
 
